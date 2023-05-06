8888c96d073fded62fb63f7d1c97911e136a771b
Deepak Achariya <deepak.achariya@sjsu.edu>
2023-05-06 14:46:46 -0700

    Added syn scripts. Updated state machine and adder input to tackle fifo_empty

--- firc.v
+++ firc.v
@@ -25,29 +25,28 @@ module firc (input  wire               Clk,
   reg signed [23:0] shift_reg_i[28:0];
   reg signed [23:0] shift_reg_q[28:0];
 
+  reg signed [23:0] data_i_0_loc_a, data_i_1_loc_a, data_i_2_loc_a,  data_i_3_loc_a, data_i_4_loc_a;
+  reg signed [23:0] data_q_0_loc_a, data_q_1_loc_a, data_q_2_loc_a,  data_q_3_loc_a, data_q_4_loc_a;
+  reg signed [23:0] data_i_0_loc_b, data_i_1_loc_b, data_i_2_loc_b,  data_i_3_loc_b, data_i_4_loc_b;
+  reg signed [23:0] data_q_0_loc_b, data_q_1_loc_b, data_q_2_loc_b,  data_q_3_loc_b, data_q_4_loc_b;
+
   // Coefficient regs
   reg [26:0] CoefIMem [15:0];
   reg [26:0] CoefQMem [15:0];
 
   reg [26:0] i_coef_reg1, i_coef_reg2, i_coef_reg3, i_coef_reg4, i_coef_reg5;
   reg [26:0] q_coef_reg1, q_coef_reg2, q_coef_reg3, q_coef_reg4, q_coef_reg5;
+  reg [26:0] i_coef_reg1_d, i_coef_reg2_d, i_coef_reg3_d, i_coef_reg4_d, i_coef_reg5_d;
+  reg [26:0] q_coef_reg1_d, q_coef_reg2_d, q_coef_reg3_d, q_coef_reg4_d, q_coef_reg5_d;
 
   wire signed [23:0] shift_reg_i_0;
   wire signed [23:0] shift_reg_q_0;
 
-  reg [4:0] addr_0_loc_a, addr_1_loc_a, addr_2_loc_a,  addr_3_loc_a, addr_4_loc_a;
-  reg [4:0] addr_0_loc_b, addr_1_loc_b, addr_2_loc_b,  addr_3_loc_b, addr_4_loc_b;
-
   reg [23:0] a2m_i_0, a2m_i_1, a2m_i_2, a2m_i_3, a2m_i_4;
   reg [23:0] a2m_q_0, a2m_q_1, a2m_q_2, a2m_q_3, a2m_q_4;
 
-
-
   reg fifo_rd_en;
   wire fifo_empty;
-
-  // Separate connection for Adder_4_B as for 3rd multiply, it should be zero.
-  reg signed [23:0] addr_4_b_i, addr_4_b_q;
 
   // STATE PARAMETERS
   parameter ST_RESET = 2'b00, ST_MUL_0 = 2'b01, ST_MUL_1 = 2'b10, ST_MUL_2 = 2'b11;
@@ -65,11 +64,11 @@ module firc (input  wire               Clk,
   // Module Instances
   fifo ip_fifo(.clk(Clk), .rst(Reset), .rd(fifo_rd_en), .wr(PushIn), .write_data1(SampI), .write_data2(SampQ), .empty(fifo_empty), .full(StopIn), .read_data1(shift_reg_i_0), .read_data2(shift_reg_q_0));
 
-  pre_mult_adder addr_0 (.clk(Clk), .reset(Reset), .a_i(shift_reg_i[addr_0_loc_a]), .a_q(shift_reg_q[addr_0_loc_a]), .b_i(shift_reg_i[addr_0_loc_b]), .b_q(shift_reg_q[addr_0_loc_b]), .o_i(a2m_i_0), .o_q(a2m_q_0));
-  pre_mult_adder addr_1 (.clk(Clk), .reset(Reset), .a_i(shift_reg_i[addr_1_loc_a]), .a_q(shift_reg_q[addr_1_loc_a]), .b_i(shift_reg_i[addr_1_loc_b]), .b_q(shift_reg_q[addr_1_loc_b]), .o_i(a2m_i_1), .o_q(a2m_q_1));
-  pre_mult_adder addr_2 (.clk(Clk), .reset(Reset), .a_i(shift_reg_i[addr_2_loc_a]), .a_q(shift_reg_q[addr_2_loc_a]), .b_i(shift_reg_i[addr_2_loc_b]), .b_q(shift_reg_q[addr_2_loc_b]), .o_i(a2m_i_2), .o_q(a2m_q_2));
-  pre_mult_adder addr_3 (.clk(Clk), .reset(Reset), .a_i(shift_reg_i[addr_3_loc_a]), .a_q(shift_reg_q[addr_3_loc_a]), .b_i(shift_reg_i[addr_3_loc_b]), .b_q(shift_reg_q[addr_3_loc_b]), .o_i(a2m_i_3), .o_q(a2m_q_3));
-  pre_mult_adder addr_4 (.clk(Clk), .reset(Reset), .a_i(shift_reg_i[addr_4_loc_a]), .a_q(shift_reg_q[addr_4_loc_a]), .b_i(addr_4_b_i),                .b_q(addr_4_b_q),                .o_i(a2m_i_4), .o_q(a2m_q_4));
+  pre_mult_adder pre_add_0 (.clk(Clk), .reset(Reset), .a_i(data_i_0_loc_a), .a_q(data_q_0_loc_a), .b_i(data_i_0_loc_b), .b_q(data_q_0_loc_b), .o_i(a2m_i_0), .o_q(a2m_q_0));
+  pre_mult_adder pre_add_1 (.clk(Clk), .reset(Reset), .a_i(data_i_1_loc_a), .a_q(data_q_1_loc_a), .b_i(data_i_1_loc_b), .b_q(data_q_1_loc_b), .o_i(a2m_i_1), .o_q(a2m_q_1));
+  pre_mult_adder pre_add_2 (.clk(Clk), .reset(Reset), .a_i(data_i_2_loc_a), .a_q(data_q_2_loc_a), .b_i(data_i_2_loc_b), .b_q(data_q_2_loc_b), .o_i(a2m_i_2), .o_q(a2m_q_2));
+  pre_mult_adder pre_add_3 (.clk(Clk), .reset(Reset), .a_i(data_i_3_loc_a), .a_q(data_q_3_loc_a), .b_i(data_i_3_loc_b), .b_q(data_q_3_loc_b), .o_i(a2m_i_3), .o_q(a2m_q_3));
+  pre_mult_adder pre_add_4 (.clk(Clk), .reset(Reset), .a_i(data_i_4_loc_a), .a_q(data_q_4_loc_a), .b_i(data_i_4_loc_b), .b_q(data_q_4_loc_b), .o_i(a2m_i_4), .o_q(a2m_q_4));
 
   ComplexMult cm_0(.clk(Clk), .reset(Reset), .data_i(a2m_i_0), .data_q(a2m_q_0), .coef_i(i_coef_reg1), .coef_q(q_coef_reg1), .mult_out_i(), .mult_out_q());
   ComplexMult cm_1(.clk(Clk), .reset(Reset), .data_i(a2m_i_1), .data_q(a2m_q_1), .coef_i(i_coef_reg2), .coef_q(q_coef_reg2), .mult_out_i(), .mult_out_q());
@@ -90,9 +89,7 @@ module firc (input  wire               Clk,
     else if(PushCoef==1)begin
       CoefIMem[CoefAddr] <= CoefI;
       CoefQMem[CoefAddr] <= CoefQ;
-
     end
-
   end
 
   // Sample Handling
@@ -169,38 +166,36 @@ module firc (input  wire               Clk,
         q_coef_reg4_d = 0;
         q_coef_reg5_d = 0;
 
-        addr_0_loc_a = 0;
-        addr_1_loc_a = 0;
-        addr_2_loc_a = 0;
-        addr_3_loc_a = 0;
-        addr_4_loc_a = 0;
+        data_i_0_loc_a = 0;
+        data_i_1_loc_a = 0;
+        data_i_2_loc_a = 0;
+        data_i_3_loc_a = 0;
+        data_i_4_loc_a = 0;
 
-        addr_0_loc_b = 0;
-        addr_1_loc_b = 0;
-        addr_2_loc_b = 0;
-        addr_3_loc_b = 0;
-        addr_4_loc_b = 0;
+        data_q_0_loc_a = 0;
+        data_q_1_loc_a = 0;
+        data_q_2_loc_a = 0;
+        data_q_3_loc_a = 0;
+        data_q_4_loc_a = 0;
+
+        data_i_0_loc_b = 0;
+        data_i_1_loc_b = 0;
+        data_i_2_loc_b = 0;
+        data_i_3_loc_b = 0;
+        data_i_4_loc_b = 0;
+
+        data_q_0_loc_b = 0;
+        data_q_1_loc_b = 0;
+        data_q_2_loc_b = 0;
+        data_q_3_loc_b = 0;
+        data_q_4_loc_b = 0;
 
         fifo_rd_en = 0;
-        addr_4_b_i = 24'b0;
-        addr_4_b_q = 24'b0;
 
         next_state = ST_MUL_0;
       end
       ST_MUL_0 :
       begin
-        addr_0_loc_a = 0;
-        addr_1_loc_a = 1;
-        addr_2_loc_a = 2;
-        addr_3_loc_a = 3;
-        addr_4_loc_a = 4;
-
-        addr_0_loc_b = 28;
-        addr_1_loc_b = 27;
-        addr_2_loc_b = 26;
-        addr_3_loc_b = 25;
-        addr_4_loc_b = 24;
-
         i_coef_reg1_d = CoefIMem[0];
         i_coef_reg2_d = CoefIMem[1];
         i_coef_reg3_d = CoefIMem[2];
@@ -213,12 +208,66 @@ module firc (input  wire               Clk,
         q_coef_reg4_d = CoefQMem[3];
         q_coef_reg5_d = CoefQMem[4];
 
+        if(fifo_empty)
+        begin
+          data_i_0_loc_a = 0;
+          data_i_1_loc_a = 0;
+          data_i_2_loc_a = 0;
+          data_i_3_loc_a = 0;
+          data_i_4_loc_a = 0;
 
-        fifo_rd_en = 1;
-        addr_4_b_i = shift_reg_i[addr_4_loc_b];
-        addr_4_b_q = shift_reg_q[addr_4_loc_b];
+          data_q_0_loc_a = 0;
+          data_q_1_loc_a = 0;
+          data_q_2_loc_a = 0;
+          data_q_3_loc_a = 0;
+          data_q_4_loc_a = 0;
 
-        next_state = ST_MUL_1;
+          data_i_0_loc_b = 0;
+          data_i_1_loc_b = 0;
+          data_i_2_loc_b = 0;
+          data_i_3_loc_b = 0;
+          data_i_4_loc_b = 0;
+
+          data_q_0_loc_b = 0;
+          data_q_1_loc_b = 0;
+          data_q_2_loc_b = 0;
+          data_q_3_loc_b = 0;
+          data_q_4_loc_b = 0;
+
+          fifo_rd_en = 0;
+
+          next_state = ST_MUL_0;
+        end
+        else
+        begin
+          data_i_0_loc_a = shift_reg_i[0];
+          data_i_1_loc_a = shift_reg_i[1];
+          data_i_2_loc_a = shift_reg_i[2];
+          data_i_3_loc_a = shift_reg_i[3];
+          data_i_4_loc_a = shift_reg_i[4];
+
+          data_q_0_loc_a = shift_reg_q[28];
+          data_q_1_loc_a = shift_reg_q[27];
+          data_q_2_loc_a = shift_reg_q[26];
+          data_q_3_loc_a = shift_reg_q[25];
+          data_q_4_loc_a = shift_reg_q[24];
+
+          data_i_0_loc_b = shift_reg_i[0];
+          data_i_1_loc_b = shift_reg_i[1];
+          data_i_2_loc_b = shift_reg_i[2];
+          data_i_3_loc_b = shift_reg_i[3];
+          data_i_4_loc_b = shift_reg_i[4];
+                                         
+          data_q_0_loc_b = shift_reg_q[28];
+          data_q_1_loc_b = shift_reg_q[27];
+          data_q_2_loc_b = shift_reg_q[26];
+          data_q_3_loc_b = shift_reg_q[25];
+          data_q_4_loc_b = shift_reg_q[24];
+
+          fifo_rd_en = 1;
+
+          next_state = ST_MUL_1;
+        end
       end
       ST_MUL_1 :
       begin
@@ -234,21 +283,31 @@ module firc (input  wire               Clk,
         q_coef_reg4_d = CoefQMem[8];
         q_coef_reg5_d = CoefQMem[9];
 
-        addr_0_loc_a = 5;
-        addr_1_loc_a = 6;
-        addr_2_loc_a = 7;
-        addr_3_loc_a = 8;
-        addr_4_loc_a = 9;
+        data_i_0_loc_a = shift_reg_i[5];
+        data_i_1_loc_a = shift_reg_i[6];
+        data_i_2_loc_a = shift_reg_i[7];
+        data_i_3_loc_a = shift_reg_i[8];
+        data_i_4_loc_a = shift_reg_i[9];
 
-        addr_0_loc_b = 23;
-        addr_1_loc_b = 22;
-        addr_2_loc_b = 21;
-        addr_3_loc_b = 20;
-        addr_4_loc_b = 19;
+        data_q_0_loc_a = shift_reg_q[23];
+        data_q_1_loc_a = shift_reg_q[22];
+        data_q_2_loc_a = shift_reg_q[21];
+        data_q_3_loc_a = shift_reg_q[20];
+        data_q_4_loc_a = shift_reg_q[19];
+
+        data_i_0_loc_b = shift_reg_i[5];
+        data_i_1_loc_b = shift_reg_i[6];
+        data_i_2_loc_b = shift_reg_i[7];
+        data_i_3_loc_b = shift_reg_i[8];
+        data_i_4_loc_b = shift_reg_i[9];
+                                       
+        data_q_0_loc_b = shift_reg_q[23];
+        data_q_1_loc_b = shift_reg_q[22];
+        data_q_2_loc_b = shift_reg_q[21];
+        data_q_3_loc_b = shift_reg_q[20];
+        data_q_4_loc_b = shift_reg_q[19];
 
         fifo_rd_en = 0;
-        addr_4_b_i = shift_reg_i[addr_4_loc_b];
-        addr_4_b_q = shift_reg_q[addr_4_loc_b];
 
         next_state = ST_MUL_2;
       end
@@ -266,21 +325,31 @@ module firc (input  wire               Clk,
         q_coef_reg4_d = CoefQMem[13];
         q_coef_reg5_d = CoefQMem[14];
 
-        addr_0_loc_a = 10;
-        addr_1_loc_a = 11;
-        addr_2_loc_a = 12;
-        addr_3_loc_a = 13;
-        addr_4_loc_a = 14;
+        data_i_0_loc_a = shift_reg_i[10];
+        data_i_1_loc_a = shift_reg_i[11];
+        data_i_2_loc_a = shift_reg_i[12];
+        data_i_3_loc_a = shift_reg_i[13];
+        data_i_4_loc_a = shift_reg_i[14];
 
-        addr_0_loc_b = 18;
-        addr_1_loc_b = 17;
-        addr_2_loc_b = 16;
-        addr_3_loc_b = 15;
-        addr_4_loc_b = 14;
+        data_q_0_loc_a = shift_reg_q[18];
+        data_q_1_loc_a = shift_reg_q[17];
+        data_q_2_loc_a = shift_reg_q[16];
+        data_q_3_loc_a = shift_reg_q[15];
+        data_q_4_loc_a = shift_reg_q[14];
+
+        data_i_0_loc_b = shift_reg_i[10];
+        data_i_1_loc_b = shift_reg_i[11];
+        data_i_2_loc_b = shift_reg_i[12];
+        data_i_3_loc_b = shift_reg_i[13];
+        data_i_4_loc_b = 24'b0;
+                                       
+        data_q_0_loc_b = shift_reg_q[18];
+        data_q_1_loc_b = shift_reg_q[17];
+        data_q_2_loc_b = shift_reg_q[16];
+        data_q_3_loc_b = shift_reg_q[15];
+        data_q_4_loc_b =24'b0;
 
         fifo_rd_en = 0;
-        addr_4_b_i = 24'b0;
-        addr_4_b_q = 24'b0;
 
         next_state = ST_MUL_0;
       end
@@ -420,10 +489,15 @@ module ComplexMult(input                    clk,
                    output reg signed [53:0] mult_out_i,
                    output reg signed [53:0] mult_out_q);
 
-  DW02_mult_2_stage #(27,27) AC(.A({data_i[23], data_i[23], data_i[23], data_i}), .B(coef_i), .TC(1'b1), .CLK(clk), .PRODUCT());
-  DW02_mult_2_stage #(27,27) BD(.A({data_q[23], data_q[23], data_q[23], data_q}), .B(coef_q), .TC(1'b1), .CLK(clk), .PRODUCT());
-  DW02_mult_2_stage #(27,27) BC(.A({data_q[23], data_q[23], data_q[23], data_q}), .B(coef_i), .TC(1'b1), .CLK(clk), .PRODUCT());
-  DW02_mult_2_stage #(27,27) AD(.A({data_i[23], data_i[23], data_i[23], data_i}), .B(coef_q), .TC(1'b1), .CLK(clk), .PRODUCT());
+  reg signed [53:0] prod_ac, prod_bd, prod_bc, prod_ad;
+
+  DW02_mult_2_stage #(27,27) AC(.A({data_i[23], data_i[23], data_i[23], data_i}), .B(coef_i), .TC(1'b1), .CLK(clk), .PRODUCT(prod_ac));
+  DW02_mult_2_stage #(27,27) BD(.A({data_q[23], data_q[23], data_q[23], data_q}), .B(coef_q), .TC(1'b1), .CLK(clk), .PRODUCT(prod_bd));
+  DW02_mult_2_stage #(27,27) BC(.A({data_q[23], data_q[23], data_q[23], data_q}), .B(coef_i), .TC(1'b1), .CLK(clk), .PRODUCT(prod_bc));
+  DW02_mult_2_stage #(27,27) AD(.A({data_i[23], data_i[23], data_i[23], data_i}), .B(coef_q), .TC(1'b1), .CLK(clk), .PRODUCT(prod_ad));
+
+  assign mult_out_i = prod_ac - prod_bd; 
+  assign mult_out_q = prod_bc + prod_ad;
 
 endmodule
 
